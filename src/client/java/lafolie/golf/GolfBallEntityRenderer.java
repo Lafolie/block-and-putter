cf377169416815b363b3d76c45e9a196e61c34f8
Lafolie <lafoliex7@aol.com>
2023-07-19 22:41:15 +0100

    added GolfBallEntityRenderer (forgot to save!)

--- src/client/java/lafolie/golf/GolfBallEntityRenderer.java
+++ src/client/java/lafolie/golf/GolfBallEntityRenderer.java
@@ -2,8 +2,10 @@ package lafolie.golf;
 
 import lafolie.golf.entities.GolfBallEntity;
 import lafolie.golf.util.GolfIdentifier;
+import net.minecraft.client.render.VertexConsumerProvider;
 import net.minecraft.client.render.entity.EntityRenderer;
 import net.minecraft.client.render.entity.EntityRendererFactory.Context;
+import net.minecraft.client.util.math.MatrixStack;
 import net.minecraft.util.Identifier;
 
 public class GolfBallEntityRenderer extends EntityRenderer<GolfBallEntity>
@@ -20,5 +22,14 @@ public class GolfBallEntityRenderer extends EntityRenderer<GolfBallEntity>
 		// TODO Auto-generated method stub
 		return GolfIdentifier.content("textures/entity/golf_ball/golf_ball.png");
 	}
+
+	@Override
+	public void render(GolfBallEntity entity, float yaw, float tickDelta, MatrixStack matrices, VertexConsumerProvider vertexConsumers, int light)
+	{
+		super.render(entity, yaw, tickDelta, matrices, vertexConsumers, light);
+
+		
+	}
+	
 	
 }
