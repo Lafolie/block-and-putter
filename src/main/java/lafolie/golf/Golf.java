501e0fa3165b722cf566f3d8c9d7adf0e29ad664
Lafolie <lafoliex7@aol.com>
2023-07-19 11:00:22 +0100

    rename exmaples

---
+++ src/main/java/lafolie/golf/Golf.java
@@ -0,0 +1,18 @@
+package lafolie.golf;
+
+import net.fabricmc.api.ModInitializer;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class Golf implements ModInitializer
+{
+	public static final String MODID = "block-and-putter";
+	public static final Logger LOG = LoggerFactory.getLogger(MODID);
+
+	@Override
+	public void onInitialize()
+	{
+	
+	}
+}
