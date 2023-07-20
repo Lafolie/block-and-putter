897a001dbbb516e19a071dd8a1252db4de11d2e4
Lafolie <lafoliex7@aol.com>
2023-07-20 16:50:44 +0100

    clubs can detect golf balls

--- src/client/java/lafolie/golf/GolfBallEntityRenderer.java
+++ src/client/java/lafolie/golf/GolfBallEntityRenderer.java
@@ -2,6 +2,11 @@ package lafolie.golf;
 
 import lafolie.golf.entities.GolfBallEntity;
 import lafolie.golf.util.GolfIdentifier;
+
+import net.minecraft.client.model.ModelPart;
+import net.minecraft.client.render.OverlayTexture;
+import net.minecraft.client.render.RenderLayer;
+import net.minecraft.client.render.VertexConsumer;
 import net.minecraft.client.render.VertexConsumerProvider;
 import net.minecraft.client.render.entity.EntityRenderer;
 import net.minecraft.client.render.entity.EntityRendererFactory.Context;
@@ -10,10 +15,12 @@ import net.minecraft.util.Identifier;
 
 public class GolfBallEntityRenderer extends EntityRenderer<GolfBallEntity>
 {
-
+	private ModelPart base;
+	private Identifier ID = GolfIdentifier.content("golf_ball");
 	protected GolfBallEntityRenderer(Context ctx)
 	{
 		super(ctx);
+		base = ctx.getPart(GolfClient.GOLF_BALL_MODEL_LAYER);
 	}
 
 	@Override
@@ -26,9 +33,9 @@ public class GolfBallEntityRenderer extends EntityRenderer<GolfBallEntity>
 	@Override
 	public void render(GolfBallEntity entity, float yaw, float tickDelta, MatrixStack matrices, VertexConsumerProvider vertexConsumers, int light)
 	{
+		VertexConsumer vertices = vertexConsumers.getBuffer(RenderLayer.getEntityCutout(getTexture(entity)));
+		base.render(matrices, vertices, light, OverlayTexture.DEFAULT_UV, 1, 1, 1, 1);
 		super.render(entity, yaw, tickDelta, matrices, vertexConsumers, light);
-
-		
 	}
 	
 	
--- src/main/java/lafolie/golf/items/GolfClub.java
+++ src/main/java/lafolie/golf/items/GolfClub.java
@@ -1,11 +1,17 @@
 package lafolie.golf.items;
 
+import java.util.List;
+
 import lafolie.golf.Golf;
+import lafolie.golf.entities.GolfBallEntity;
+import net.minecraft.entity.Entity;
 import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemStack;
 import net.minecraft.util.Hand;
 import net.minecraft.util.TypedActionResult;
+import net.minecraft.util.math.Box;
+import net.minecraft.util.math.Vec3d;
 import net.minecraft.world.World;
 
 public class GolfClub extends Item
@@ -23,7 +29,12 @@ public class GolfClub extends Item
 	@Override
 	public TypedActionResult<ItemStack> use(World world, PlayerEntity playerEnt, Hand hand)
 	{
-		GolfBall ball = findBall(world, playerEnt);
+		if(world.isClient())
+		{
+			return super.use(world, playerEnt, hand);
+		}
+
+		GolfBallEntity ball = findBall(world, playerEnt);
 
 		if(ball != null)
 		{
@@ -36,8 +47,24 @@ public class GolfClub extends Item
 		}
 	}
 
-	private GolfBall findBall(World world, PlayerEntity playerEnt)
+	private Box getCollisionBox(PlayerEntity playerEnt)
 	{
+		Vec3d pos = playerEnt.getPos();
+		Vec3d forward = Vec3d.fromPolar(0f, playerEnt.getYaw());
+		return Box.of(pos.add(forward.multiply(0.5d)), 1.5d, 1.5d, 1.5d);
+	}
+
+	private GolfBallEntity findBall(World world, PlayerEntity playerEnt)
+	{
+		List<Entity> entities = world.getOtherEntities(playerEnt, getCollisionBox(playerEnt));
+		for(Entity ent : entities)
+		{
+			Golf.LOG.info("Found ent {}", ent.toString());
+			if(ent instanceof GolfBallEntity)
+			{
+				return (GolfBallEntity)ent;
+			}
+		}
 		return null;
 	}
 }
